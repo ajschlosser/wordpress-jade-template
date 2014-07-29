8fd6169d3adc206cc1c6187d8895a8a5dda6b3c6
Aaron John Schlosser <aaron@aaronschlosser.com>
2014-07-28 20:48:49 -0700

    finish creating .jade files for each original .php file

--- tag.php
+++ tag.php
@@ -1,12 +1,1 @@
-<?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Tag Archives: ', 'blankslate' ); ?><?php single_tag_title(); ?></h1>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_header(); ?><section role="main"><header><h1><?php _e( 'Tag Archives: ', 'blankslate' ); single_tag_title(); ?></h1></header><?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?><?php get_template_part( 'entry' ); ?><?php endwhile; endif; ?><?php get_template_part( 'nav', 'below' ); ?></section><?php get_sidebar(); ?><?php get_footer(); ?>
