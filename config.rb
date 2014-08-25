5fc4cde7f7dd514a89158b7b6ee7c2bf9c8055fb
Aaron John Schlosser <aaron@aaronschlosser.com>
2014-08-25 08:43:30 -0700

    begin updating front and back-end dependencies

--- config.rb
+++ config.rb
@@ -1,9 +1,10 @@
 # Require any additional compass plugins here.
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
 
--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,13 @@
 
 var gulp 				= require("gulp"),
 	gutil 				= require("gulp-util"),
-	uglify 				= require("gulp-uglify"),
 	concat				= require("gulp-concat"),
 	watch 				= require("gulp-watch"),
 	compass 			= require("gulp-compass"),
 	jade 				= require("gulp-jade-php"),
-	minifyCSS 			= require("gulp-minify-css"),
-	plumber				= require("gulp-plumber"),
-	imagemin 			= require("gulp-imagemin");
+	plumber				= require("gulp-plumber")
 
 var paths = {
-	scripts: {
-		src: ["./js/**/*.js", "!./js/**/*.min.js"],
-		dest: "./js",
-		con: "all.min.js"
-	},
 	styles: {
 		src: "./sass/**/*.scss",
 		dest: "./stylesheets"
@@ -29,59 +21,30 @@ var paths = {
 	templates: {
 		src: "./templates/**/*.jade",
 		dest: "./"
-	},
-	images: {
-		src: "./images/**/*",
-		dest: "./images"
 	}
 };
 
-gulp.task("scripts", function() {
-  return gulp.src(paths.scripts.src)
-  	.pipe(plumber())
-	.pipe(uglify())
-	.pipe(concat(paths.scripts.con))
-	.pipe(plumber.stop())
-	.pipe(gulp.dest(paths.scripts.dest))
-});
-
 gulp.task("styles", function() {
-  return gulp.src(paths.styles.src)
-  .pipe(plumber())
-  .pipe(compass({
-	css: "./stylesheets",
-	sass: "./sass",
-	image: "./images"
-  }))
-  .pipe(minifyCSS())
-  .pipe(plumber.stop())
-  .pipe(gulp.dest(paths.styles.dest));
+	return gulp.src(paths.styles.src)
+		.pipe(plumber())
+		.pipe(compass({
+			css: "./stylesheets",
+			sass: "./sass",
+			image: "./images"
+		}))
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(paths.styles.dest));
 });
 
 gulp.task("templates", function() {
   gulp.src("./templates/**/*.jade")
   	.pipe(plumber())
-	.pipe(jade({
-		locals: {
-		  title: "OMG THIS IS THE TITLE"
-		}
-	 }))
+	.pipe(jade())
 	.pipe(plumber.stop())		
 	.pipe(gulp.dest("./"));
 });
 
-gulp.task("imagemin", function () {
-	return gulp.src("./images/*.*")
-		.pipe(plumber())
-		.pipe(imagemin())
-		.pipe(plumber.stop())
-		.pipe(gulp.dest("./images"));
-});
-
-
 gulp.task("default", function() {
-	gulp.watch(paths.scripts.src, ["scripts"]);
 	gulp.watch(paths.styles.src, ["styles"]);
-	gulp.watch(paths.images.src, ["imagemin"]);
 	gulp.watch(paths.templates.src, ["templates"]);
 });
